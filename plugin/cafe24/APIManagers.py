5c1f510debc4044ca204fed575e6315881e58ed9
smlee <smlee@dblab.postech.ac.kr>
2021-06-29 07:59:00 +0000

    Add to upload addtional image up to 20

--- plugin/cafe24/APIManagers.py
+++ plugin/cafe24/APIManagers.py
@@ -632,6 +632,8 @@ class Cafe24Manager:
     def update_additional_images(self, product_no, links):
         if len(links) == 0:
             return None
+        if len(links) > 20:
+            links = links[0:20]
         cnt = 0
         max_try = 3
         while cnt < max_try:
@@ -1190,7 +1192,7 @@ class Cafe24Manager:
                             pass
 
 
-            if len(additional_image) > 0:
+            if len(additional_image) > 0
                 response = self.update_additional_images(tpid, additional_image)
                 if 'error' in response:
                     print_flushed("Product update was successful, but additional image update failed")
